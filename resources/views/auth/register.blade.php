008efa562040146a08298b3ca17b1758dcb70060
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-07-18 23:39:57 +0100

    Routes to register and home with dinamic views

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    
+    <h1>Register new User</h1>
+
+    <x-test2>
+    </x-test2>
+
+</x-layout>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,10 +1,9 @@
 <x-layout>
+    
     <h1>Hello, welcome page</h1>
+    {{-- <p>{{ route('register')}}</p> --}}
 
     <x-test2>
     </x-test2>
 
-
-
-   
 </x-layout>
