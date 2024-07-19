f46a86ea4985113ac2a51258611124793af000fc
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-07-19 15:06:55 +0100

    register working and saving into DB

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 // Request is used to grab data
+
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -13,10 +15,18 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         // VALIDATE
-        $request->validate([
+        $fields = $request->validate([
             'username' => ['required','max:255'],
             'email' => ['required','max:255', 'email'],
             'password' => ['required','max:255', 'confirmed'],
         ]);
+
+        // dd($fields);
+
+        // REGISTER - takes an array username => $request->username
+        User::create($fields);
+
+        dd('ok');
+
     }
 }
